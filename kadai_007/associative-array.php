e2d53316760fb6f178fe4e269b0a666b42b985d3
saitou-ryou <145634271+saitou-ryou@users.noreply.github.com>
2023-12-30 20:36:39 +0900

    add a file

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+
+</head>
+
+<body>
+  <p>
+    <?php
+
+    $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($array);
+    
+    ?>
+  </p>
+</body>
