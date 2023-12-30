f95cf0da00c48e96da984d446db57ac9af6a7862
saitou-ryou <145634271+saitou-ryou@users.noreply.github.com>
2023-12-30 13:59:37 +0900

    add a file

---
+++ kadai_005/score.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_005</title>
+</head>
+
+<body>
+  <p>
+    <?php
+      $number = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+      
+      echo $number/10;
+    ?>
+    <?php
+      $number = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+      
+      echo $number;
+      ?>
+</body>
