59a9faf94bc64d8c75267d1d93297f0e9e259839
saitou-ryou <145634271+saitou-ryou@users.noreply.github.com>
2024-01-16 10:22:40 +0900

    add three files

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,74 @@
+<?php
+
+session_start();
+
+$name = $_POST['employee_name'];
+$age = $_POST['employee_age'];
+$department = $_POST['department'];
+
+$errors = [];
+
+if (empty($name) ) {
+  $errors[] = 'お名前を入力してください。';
+}
+
+if (!is_numeric($age) ) {
+  exit('年齢には数値を入力してください。');
+}
+
+if (empty($errors)) {
+  
+  $_SESSION['employee_name'] = $name;
+  $_SESSION['employee_age'] = $age;
+  $_SESSION['department'] = $department;
+
+  setcookie('name', $name, time() + 3600 );
+  setcookie('age', $age, time() + 3600 );
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <h2>入力内容をご確認ください。</h2>
+  <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+
+  <table border="1">
+    <tr>
+      <th>項目</th>
+      <th>入力内容</th>
+    </tr>
+    <tr>
+      <td>社員名</td>
+      <td><?php echo $name; ?></td>
+    </tr>
+    <tr>
+      <td>年齢</td>
+      <td><?php echo $age; ?></td>
+    </tr>
+    <tr>
+      <td>所属部署</td>
+      <td><?php echo $department; ?></td>
+    </tr>
+  </table>
+
+  <p>
+    <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+    <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+  </p>
+  <?php
+  if (!empty($errors)) {
+    foreach ($errors as $error) {
+      echo '<font color="red">' . $error . '</font>' . '<br>';
+    }
+
+    echo '<script>document.getElementById("confirm-btn").disabled = true; </script>';
+  }
+  ?>
+</body>
+
+</html>
